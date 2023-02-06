7787ebef3bdf5660f4910f332f0dded9f7731646
erenilda <erenyldajsilva@gmail.com>
2023-02-06 13:29:19 -0300

    exemplos diversos em java

---
+++ tipos-variaveis/src/Incrementar.java
@@ -0,0 +1,13 @@
+public class Incrementar {
+    public static void main(String[] args) {
+     int numero = 5;
+        //x repeticao 
+        
+        //numero =numero +1;
+
+        System.out.println( ++ numero);
+
+        System.out.println(numero);
+
+    }
+}
---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,26 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.33;
+
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+    }
+}
